990396a005a750f5014e6353fb2290b780fe2809
HDZLUNA <hjahirezequiel@gmail.com>
2024-12-02 10:49:38 -0600

    Add files via upload
    Laboratorios.1 20/08/24

---
+++ LABORATORIOS/lab 1.R
@@ -0,0 +1,36 @@
+#jahir ezequiel hernandez luna
+
+# lboratorio 1
+# gastos totales
+300+240+1527+400+1500+1833
+
+celular <- 300
+celular
+
+# raiz cuadrada
+sqrt(9)
+
+celular <- 300
+CELULAR <- 800
+
+CELULAR - celular
+CELULAR + celular
+
+#Documentacion de ayuda
+help(abs)
+help(mean)
+
+#Autoevaluacion
+
+celular <- 200
+transporte <- 350
+comestibles <- 170
+alquiler <- 450
+gimnasio <- 150
+otros <- 250
+
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+
+
+
+
